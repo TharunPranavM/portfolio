41c67385a726e441be897573c55b338c387ea677
TharunPranavM <137869971+TharunPranavM@users.noreply.github.com>
2024-08-20 21:48:02 +0530

    Add files via upload

---
+++ profile.js
@@ -0,0 +1,19 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Sticky Navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
